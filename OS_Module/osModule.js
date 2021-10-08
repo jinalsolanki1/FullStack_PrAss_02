eaffa0659cad38246bec838baba0b2270a41a5bc
jinalsolanki1 <91380043+jinalsolanki1@users.noreply.github.com>
2021-10-08 22:08:32 +0530

    Add files via upload

--- OS_Module/osModule.js
+++ OS_Module/osModule.js
@@ -1,1 +1,7 @@
-
+var os=require('os');
+console.log("1.CPU Architecture :"+ os.arch());
+console.log("2.Free Memory :"+ os.freemem());
+console.log("3.Total Memory :"+ os.totalmem());
+console.log("4.OS Platform :"+ os.platform());
+console.log("5.Current User Information :");
+console.log(os.userInfo());
